ebf5b20d1e00266da7f4b3b49607f00fc224077f
John Michael Balatico <john.balatico@carbonetes.com>
2025-01-12 23:37:59 +0800

    edited Cors Allowed Origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -69,7 +69,7 @@ CHANNEL_LAYERS = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Example: Frontend URL
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://em-chat-three.vercel.app",
